2b9b192a9ae817cf5ae7f14cdca7580369fd888c
jjanmo <jjanmo07@hanmail.net>
2023-07-22 13:36:38 +0900

    fix: cleanup code

--- js-part/quiz/03.js
+++ js-part/quiz/03.js
@@ -1,41 +1,64 @@
-// 반복문을 통한 여러 개의 비동기 처리
 const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
 const postIds = [1, 2, 5];
 
 // 1개의 데이터를 받아오는 방법
-// async function fetchPost() {
-//   const response = await fetch(`${BASE_URL}/2`);
-//   const result = await response.json();
-//   console.log(result);
-// }
+(async function fetchPost() {
+  const response = await fetch(`${BASE_URL}/2`);
+  const result = await response.json();
+  console.log('1', result);
+})();
 
-// fetchPost();
+(function fetchPost() {
+  fetch(`${BASE_URL}/2`).then((res) => res.json().then((data) => console.log('2', data)));
+})();
 
+// 여러 개의 비동기 처리 방법들
 // sol1)
 (async function fetchPosts() {
   const results = [];
   for (let i = 0; i < postIds.length; i++) {
+    console.log(`시작 ${i}`);
     const response = await fetch(`${BASE_URL}/${postIds[i]}`);
     const result = await response.json();
     results.push(result);
+    console.log(`끝 ${i}`);
   }
 
-  // console.log('sol1', results);
+  console.log('sol1', results);
 })();
 
-// sol2)
+// sol2-1) map
 (async function fetchPosts() {
-  const results = [];
-  postIds.forEach(async (id) => {
+  const results = postIds.map(async (id) => {
+    console.log(`${id} 시작`);
     const response = await fetch(`${BASE_URL}/${id}`);
     const result = await response.json();
-    results.push(result);
+    console.log('fetched id', result.id);
+
+    console.log(`${id} 끝`);
+    return result;
   });
 
-  // console.log('sol2', results);
+  console.log('map', results); // Promise[] → 콜백함수가 async/await가 없는 것처럼 작동
 })();
 
-// sol3)
+// sol2-2) foreach
+//
+(async function fetchPosts() {
+  const results = [];
+  postIds.forEach(
+    async (id) =>
+      await fetch(`${BASE_URL}${id}`).then((res) =>
+        res.json().then((data) => {
+          results.push(data);
+        })
+      )
+  );
+
+  console.log('foreach', results); // []  → 콜백함수가 async/await가 없는 것처럼 작동, 콜백함수만 실행하고 forEach의 임무는 끝남, 콜백함수 내부의 코드가 작동할때까지 기다리지 않음
+})();
+
+// sol3-1)
 (async function fetchPosts() {
   const promises = postIds.map((id) => fetch(`${BASE_URL}/${id}`));
   const results = await Promise.allSettled(promises)
@@ -47,38 +70,30 @@ const postIds = [1, 2, 5];
         results.push(result);
       }
       return results;
-      // return responses.map(async (response) => await response.json()); // 이렇게 변경되면 안되는 이유! → array method안에서 async-await를 사용하지 못하는 이유
     });
 
-  // console.log('allSettled', results);
+  console.log(results);
+})();
+
+// sol3-2) : Promise.allSettled를 2번 사용할 수도 있다!!
+(async function fetchPosts() {
+  const promises = postIds.map((id) => fetch(`${BASE_URL}/${id}`));
+  const results = await Promise.allSettled(promises)
+    .then((responses) => responses.map((response) => response.value))
+    .then((responses) => Promise.allSettled(responses.map((response) => response.json())))
+    .then((posts) => posts.map((p) => p.value));
+
+  console.log(results);
 })();
 
 // sol4)
 (async function fetchPosts() {
   const results = await Promise.allSettled(
-    postIds.map(
-      (id) => fetch(`${BASE_URL}/${id}`).then((res) => res.json())
-      // .then((data) => data)
+    postIds.map((id) =>
+      fetch(`${BASE_URL}/${id}`)
+        .then((res) => res.json())
+        .then((data) => data)
     )
   );
   console.log(results);
 })();
-
-// Q. fetch가 잘 될까??
-// A. 배열 메소드 안의 콜백에서는 async-await가 동작하지 않음!
-// function fetchPosts() {
-//   postIds.forEach(async (id) => await fetch(`${BASE_URL}${id}`).then((res) => console.log(res.json())));
-// }
-
-// fetchPosts();
-
-// async function fetchPosts() {
-//   const promises = postIds.map((id) => fetch(`${BASE_URL}/${id}`));
-//   const results = await Promise.allSettled(promises).then((responses) =>
-//     responses.map((res) => res.value.body.then((body) => body.json()))
-//   );
-
-//   console.log(results);
-// }
-
-// fetchPosts();
